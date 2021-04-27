84027654d779ddb6f50fcc871a5c186537a84caa
Salis1998 <38522250+Salis1998@users.noreply.github.com>
2021-04-27 06:38:48 +0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-LogEntry.objects.all().delete()
 # Customize the way the admin panel looks
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')  # displays the properties mentioned in the tuple
